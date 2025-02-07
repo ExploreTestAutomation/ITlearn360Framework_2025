1bd0034712f0a2a8ee984edf4c67c983fffae38b
unknown <aarti.a.singh01@gmail.com>
2025-02-07 17:49:10 +0530

    test

---
+++ src/test/java/com/itlearn/testcases/testCaseDemo.java
@@ -0,0 +1,17 @@
+package com.itlearn.testcases;
+
+import org.testng.annotations.Test;
+
+import com.itlearn.pageobject.BaseTest;
+
+public class testCaseDemo extends BaseTest {
+	@Test
+	void testcheck() {
+		
+		BaseTest obj = new BaseTest();
+		
+		
+		
+	}
+
+}
